59008543873d4e3a03c030fccef87a00159f86f8
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-17 00:46:48 +0700

    Add route [albums] to get all albums from database

--- server.js
+++ server.js
@@ -3,6 +3,35 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+// Database
+const albums = [
+  {
+    id: 1,
+    title: "Electric Ladyland",
+    artist: "The Jimi Hendrix Experience",
+  },
+  {
+    id: 2,
+    title: "In Through the Out Door",
+    artist: "Led Zeppelin",
+  },
+  {
+    id: 3,
+    title: "Some Girls",
+    artist: "The Rolling Stone",
+  },
+];
+
+// app
+app.get("/albums", (req, res) => {
+  res.status(200).json({
+    status: "Succeeded",
+    data: albums,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is started at P ort ${PORT}`);
 });
